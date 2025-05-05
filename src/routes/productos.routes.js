1a53f7772831e7177c29d2448f121ecdcfeeb042
MoisesDV-coder <numoises6@gmail.com>
2025-05-05 17:01:14 -0600

    Manejo de productos
    añadidos
    - agregar, editar y eliminar productos
    - ruta a proveedores y productos que no estaban

---
+++ src/routes/productos.routes.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import path from 'path';
+
+const router = express.Router();
+const __dirname = import.meta.dirname;
+
+router.get('/productos', (req, res) => {
+    res.sendFile(path.join(__dirname, '../views/productos.html'));
+});
+
+export default router;
