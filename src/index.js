c0d12803eb0d3a1c551ec337a657abfdd549277b
Obed-Is <obedaquino71@gmail.com>
2025-05-19 21:29:30 -0600

    Se cambio la runa inicial del proyecto de 'inicio' a 'proveedores'

--- src/index.js
+++ src/index.js
@@ -4,5 +4,5 @@ import { PORT } from './config.js';
 //desde aqui se inicia el servidor/web, ademas al importar el config.js se inicializa la conexion a la base de datos
 // 2 pajaros de un tiro U_U
 app.listen(PORT, () => {
-    console.log(`Server activo: http://localhost:${PORT}/inicio`);
+    console.log(`Server activo: http://localhost:${PORT}/productos`);
 })
